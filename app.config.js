6f2ef711919ca2cba5a1aeaab892564102f75419
Uche Osuchukwu <osuchukwuuche@gmail.com>
2024-10-29 13:46:35 +0100

    updated~3  app.config.js

--- app.config.js
+++ app.config.js
@@ -1,15 +1,36 @@
-module.exports = {
-    // use the variable if it's defined, otherwise use the fallback
-    // icon: process.env.APP_ICON || './assets/icon.png',
-    // name: process.env.APP_NAME || 'My App',
+module.exports = ({ config }) => ({
+    ...config,
+    name: "AnoTrade", // Static property moved here
+    slug: "anotrade",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#fff",
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.Anotrade",
+      googleServicesFile: process.env.GOOGLE_SERVICES_INFO_PLIST, // Dynamic property
+    },
     android: {
-        googleServicesFile:process.env.GOOGLE_SERVICES_JSON,
-        // package: "com.sir_emmy_uche.anotrade",
+      package: "com.Anotrade", // Static property added here
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON, // Dynamic property
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#121212",
       },
-      ios:{
-        googleServicesFile:GOOGLE_SERVICES_INFO_PLIST,
-      }
-//     "android": {
-//     "package": "com.sir_emmy_uche.anotrade"
-//   }
-  };
+      permissions: ["android.permission.RECORD_AUDIO"],
+    },
+    extra: {
+      eas: {
+        projectId: "bf75dd29-a258-44e2-b222-cf41310a21ad",
+      },
+    },
+    owner: "sir_emmy_uche",
+  });
+  

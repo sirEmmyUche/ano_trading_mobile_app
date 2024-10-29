9a627b992ac93c0a3b1d72af6f4d7c38d8c52fea
Uche Osuchukwu <osuchukwuuche@gmail.com>
2024-10-29 13:11:32 +0100

    updated~2  app.config.js

--- app.config.js
+++ app.config.js
@@ -2,10 +2,8 @@ module.exports = {
     // use the variable if it's defined, otherwise use the fallback
     // icon: process.env.APP_ICON || './assets/icon.png',
     // name: process.env.APP_NAME || 'My App',
-    GOOGLE_SERVICES_JSON:process.env.GOOGLE_SERVICES_JSON ||'./google-services.json',
-    GOOGLE_SERVICES_INFO_PLIST:process.env.GOOGLE_SERVICES_INFO_PLIST || './GoogleService-Info.plist',
     android: {
-        googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+        googleServicesFile:process.env.GOOGLE_SERVICES_JSON,
         // package: "com.sir_emmy_uche.anotrade",
       },
       ios:{
